3a540c055fe8f272ccea10dffd5d279bd6d6b6e9
guilhreme <guibferrarezi@gmail.com>
2026-05-28 14:50:21 -0300

    feat(api): add indexes on matches.edition_id and highlights.edition_id

---
+++ api/internal/db/migrations/005_indexes.down.sql
@@ -0,0 +1,2 @@
+DROP INDEX IF EXISTS idx_highlights_edition_id;
+DROP INDEX IF EXISTS idx_matches_edition_id;
---
+++ api/internal/db/migrations/005_indexes.up.sql
@@ -0,0 +1,2 @@
+CREATE INDEX idx_matches_edition_id ON matches(edition_id);
+CREATE INDEX idx_highlights_edition_id ON highlights(edition_id);
